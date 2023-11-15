27f053c2c7403ba7536253ea2d902308c9060551
Francisco <fra.zelada.s@gmail.com>
2023-11-15 14:41:55 -0300

    .env cloudinary config

--- miniblog/post/cloudinary_utils.py
+++ miniblog/post/cloudinary_utils.py
@@ -1,20 +1,24 @@
 # cloudinary_utils.py
 from django.conf import settings
 import cloudinary
+
+
+cloudinary.config(
+        cloud_name=settings.CLOUDINARY['CLOUD_NAME'],
+        api_key=settings.CLOUDINARY['API_KEY'],
+        api_secret=settings.CLOUDINARY['API_SECRET'],
+        api_proxy = settings.CLOUDINARY['API_PROXY'],
+    )
+
 import os
 from cloudinary.uploader import upload
 
-def configure_cloudinary():
-    cloudinary.config(
-        cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-        api_key=os.getenv("CLOUDINARY_API_KEY"),
-        api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-        secure=True
-    )
+# def configure_cloudinary():
+
 
 def upload_to_cloudinary(file_path, public_id=None):
 
-    configure_cloudinary()
+    # configure_cloudinary()
 
     try:
         result = upload(file_path, public_id=public_id)
